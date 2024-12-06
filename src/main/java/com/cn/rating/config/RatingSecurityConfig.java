ec5692173f835bd49aa3ba7af42ed90460be4c43
Mukesh Kumar <ermkit@gmail.com>
2024-12-07 00:02:44 +0530

    optimising rating app for not getting api response using jwt token

--- src/main/java/com/cn/rating/config/RatingSecurityConfig.java
+++ src/main/java/com/cn/rating/config/RatingSecurityConfig.java
@@ -3,9 +3,11 @@ package com.cn.rating.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,9 +18,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.cn.rating.jwt.JwtAuthenticationFilter;
 
-@Configurable
+@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true)
 public class RatingSecurityConfig {
 	
 	
